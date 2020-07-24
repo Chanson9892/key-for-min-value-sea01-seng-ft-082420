96a720d4a3ac977cbb993770d01ae54362601be1
Chandler Hanson <chan.hanson5@gmail.com>
2020-07-24 00:55:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,10 +3,12 @@
 
 def key_for_min_value(name_hash)
   min_value = name_hash[0][0]
+  min_key = name_hash[0]
   name_hash.each do |key, value|
     if name_hash[key][value] < min_value
-      min_value = name_hash[key]
+      min_value = name_hash[key][value]
+      min_key = name_hash[key]
     end
   end
-  min_value
+  min_key
 end
